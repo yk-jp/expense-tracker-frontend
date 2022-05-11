d91df02f91eb42ce74731b1f4f449dbc31bd1696
Tomo-ja <tomonao.hj@gmail.com>
2022-05-11 16:36:15 -0700

    create header compornet

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './sass/App.scss';
 
+import Header from './componets/Header';
+
 function App() {
   return (
     <div className="App">
-      
+      <Header />
     </div>
   );
 }
---
+++ src/componets/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header(){
+	return (
+		<header>
+			<p className="test">test</p>
+		</header>
+	)
+}
+
+export default Header
