d6f72ccca034e134736d51d1b4066948e55dd962
Tomo-ja <tomonao.hj@gmail.com>
2022-05-12 21:37:35 -0700

    put content of hero area

--- src/components/LandingPage/HeroArea.tsx
+++ src/components/LandingPage/HeroArea.tsx
@@ -1,9 +1,23 @@
 import React from "react";
 
+import appStoreLogo from "../../image/LP_appStore_logo.jpeg";
+import googlePlayLogo from "../../image/LP_googlePlay_logo.jpeg";
+
 
 function HeroArea(){
 	return(
-		<div></div>
+		<main className="outer hero">
+			<h1 className="hero_title">Managing money<br /> as simple as possible</h1>
+			<div className="hero_sub">
+				<p className="hero_sub_text">
+					Get insights on your spending, set goals, and increase your savings.
+				</p>
+				<div className="hero_sub_app-download">
+					<img className="hero_sub_app-download_logo" src={appStoreLogo} alt="App Store logo" />
+					<img className="hero_sub_app-download_logo" src={googlePlayLogo} alt="google play logo" />
+				</div>
+			</div>
+		</main>
 	)
 }
 
--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import Header from "./Header";
+import HeroArea from "./HeroArea";
 
 function LandingPage(){
 	return(
 		<div className="landing-page">
 			<Header />
+			<HeroArea />
 		</div>
 	)
 }
