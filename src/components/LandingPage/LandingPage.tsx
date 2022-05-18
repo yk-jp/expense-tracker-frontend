36aa6a093dc02b8b788a4e1eb469358eb1c2b547
Tomo-ja <tomonao.hj@gmail.com>
2022-05-17 21:28:18 -0700

    animation on slide show without responsive

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import Header from "./Header";
 import HeroArea from "./HeroArea";
+import SlideShow from "./SlideShow";
 
 function LandingPage(){
 	return(
 		<div className="landing-page">
 			<Header />
 			<HeroArea />
+			<SlideShow />
 		</div>
 	)
 }
--- src/components/LandingPage/SlideShow.tsx
+++ src/components/LandingPage/SlideShow.tsx
@@ -1,8 +1,27 @@
 import React from "react";
+import picture1 from "../../image/dammy.jpeg"
+import picture2 from "../../image/dammy1.jpeg"
+import picture3 from "../../image/dammy2.jpeg"
 
 function SlideShow(){
+
+	let imagesObject = [
+		<img className="slide-show_pic" src={picture1} />, 
+		<img className="slide-show_pic" src={picture2} />, 
+		<img className="slide-show_pic" src={picture3} />
+	]
+
+
+
 	return(
-		<div></div>
+		<div className="slide-show">
+			<div className="slide-show_primary slide-show_section">
+				{imagesObject}
+			</div>
+			<div className="slide-show_support slide-show_section">
+				{imagesObject}
+			</div>
+		</div>
 	)
 }
 
