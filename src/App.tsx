f714ed773a202d407f97094c8b00aba4aa25063d
Tomo-ja <tomonao.hj@gmail.com>
2022-06-11 12:20:21 -0700

    api connection text via request token

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './sass/App.scss';
 
 import LandingPage from './components/LandingPage/LandingPage';
+import ApiConnectionTest from './components/apitTest/ApiConnection';
 
 const App = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<LandingPage />} />
+      <Route path='/apiTest' element={<ApiConnectionTest />} />
     </Routes>
   </BrowserRouter>
 );
