5af41cc76d32cb29197fe2576b7b1424eb6bac76
Tomo-ja <tomonao.hj@gmail.com>
2022-08-15 12:43:02 -0700

    add fontawsome

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,8 +1,21 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react'
 
 const Login = () => (
-  <div className=" h-4 m-7 text-yellow-600 text-3xl">
-    test
+  <div className="">
+		<div><i className='fa-solid fa-user' /></div>
+		<form>
+			<input type="email" placeholder='Email'/>
+			<input type="password" placeholder='Password' />
+			<div>
+				<label>
+					<input type="checkbox"/>
+					Remember me?
+				</label>
+				<p>Forgot Password?</p>
+			</div>
+		</form>
+		<button type='submit' onClick={()=>{console.log('here')}} >Login</button>
   </div>
 );
 
