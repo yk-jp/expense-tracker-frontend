3382d5eca007d7f1830420dc511eec3f9c149991
Tomo-ja <tomonao.hj@gmail.com>
2022-08-15 12:05:30 -0700

    delete landing page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import LandingPage from './components/LandingPage/LandingPage';
+import Login from './components/Login';
 import ApiConnectionTest from './components/apitTest/ApiConnection';
 
 const App = () => (
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<LandingPage />} />
+      <Route path="/" element={<Login />} />
       <Route path='/apiTest' element={<ApiConnectionTest />} />
     </Routes>
   </BrowserRouter>
---
+++ src/components/Login.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Login = () => (
+  <div className=" h-4 m-7 text-yellow-600 text-3xl">
+    test
+  </div>
+);
+
+export default Login;
