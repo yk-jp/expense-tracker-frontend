429af5162806ee04d73fa36141734874e0a80e65
Tomo-ja <tomonao.hj@gmail.com>
2022-08-19 14:50:56 -0700

    apply design for calender based on sample data

--- src/components/Calender.tsx
+++ src/components/Calender.tsx
@@ -1,52 +1,128 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-restricted-syntax */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import CalenderDay from './CalenderDay'
 import { getDays, getDayOfFirst, getMonthName, getDayName } from '../Utilities/date'
 
 interface transaction{
+	id: number,
+	category: string,
 	event: string,
-	amount: number,
+	amount: string,
+	memo: string,
 	date: string,
-	memo: string | null,
-	category: number
 }
 
+const sampleData: transaction[] = [
+	{
+		"id": 1,
+		"category": "some",
+		"event": "Expense",
+		"amount": "100.00",
+		"memo": "test111222",
+		"date": "2022-08-10",
+},
+{
+		"id": 2,
+		"category": "some",
+		"event": "Expense",
+		"amount": "100.00",
+		"memo": "",
+		"date": "2022-08-10",
+},
+{
+		"id": 6,
+		"category": "tip",
+		"event": "Income",
+		"amount": "200.00",
+		"memo": "",
+		"date": "2022-08-20",
+}
+]
+
 interface dayDetail{
-	day: string,
+	id: number,
+	day: number,
 	income: number
 	expense: number
 }
 
 const Calender = () => {
 
-	const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
 	const daysColorPallet = ["bg-red-500", "bg-orange-500", "bg-amber-500", "bg-lime-500", "bg-emerald-500", "bg-cyan-500", "bg-blue-500"]
-
-	const getFirstDay = (year: number, month: number): number => (
-		new Date(year, month, 1).getDay()
-	)
-
 
 	const [targetMonth, setTargetMonth] = useState<Date>(new Date())
 	const [dailyTransactions, setDailyTransactions] = useState<dayDetail[]>([])
 
-	const createTable = () => {
-		const prefixSpace = getFirstDay(targetMonth.getFullYear(), targetMonth.getMonth())
-		const temp: dayDetail[] = []
-		for(let i=0; i<prefixSpace; i+1){
-			temp.push({day: "", income: 0, expense: 0})
-		}
+	const fetchAllTransactions = () => {
+		// do request to /transaction return {"result": {"all_transactions": {}, "stats": {}}}
 	}
 
+	const reduceTransactionsByEachDay = (transactions: transaction[]): Map<number, transaction[]> => {
+		const map = new Map<number, transaction[]>()
 
-	const transactions: transaction[] = [
-		{event: "test", amount: 300, date: '2022-8-10', memo: "", category: 10},
-		{event: "test", amount: 100, date: '2022-8-11', memo: "", category: 20},
-		{event: "test", amount: 300, date: '2022-8-13', memo: "", category: 10},
-		{event: "test", amount: 200, date: '2022-8-14', memo: "", category: 20},
-		{event: "test", amount: 100, date: '2022-8-10', memo: "", category: 10}
-	]
+		for (const t of transactions) {
+			const fd = t.date.split("-")
+			const d: number = parseInt(fd[2], 10)
 
+			if (map.has(d)){
+				const prev = map.get(d)
+				prev!.push(t)
+				map.set(d, prev!)
+			} else {
+				map.set(d, [t])
+			}
+		}
+		return map
+	}	
+
+	const createTable = () => {
+		const prefixSpace = getDayOfFirst(targetMonth.getFullYear(), targetMonth.getMonth())
+		const reducedTransaction = reduceTransactionsByEachDay(sampleData)
+		const temp: dayDetail[] = []
+		let idK = 0
+		for(let i=0; i<prefixSpace; i++){
+			temp.push({id: idK, day: 0, income: -1, expense: -1})
+			idK += 1
+		}
+		const n = getDays(targetMonth.getFullYear(), targetMonth.getMonth())
+		for (let i=1; i<=n; i++ ){
+			const trans = reducedTransaction.get(i)
+			if (!trans){
+				temp.push({id: idK, day: i, income: 0, expense:0})
+				idK += 1
+			}
+			else{
+				let ic = 0
+				let ex = 0
+				for (const t of trans){
+					if (t.event === "Income"){
+						ic += parseFloat(t.amount)
+					} else {
+						ex += parseFloat(t.amount)
+					}
+				}
+				temp.push({id: idK, day: i, income: ic, expense: ex})
+				idK += 1
+			}
+		}
+		while (true){
+			console.log(temp.length)
+			if (temp.length % 7 === 0){
+				break
+			}
+			temp.push({id: idK, day: 0, income: 0, expense: 0})
+			idK += 1
+		}
+		setDailyTransactions(temp)
+	}
+
+	useEffect(()=> {
+		createTable()
+	}, [targetMonth])
+	
 
 	return(
 		<div className="w-168 mx-auto">
@@ -55,12 +131,17 @@ const Calender = () => {
 			</h2>
 			<div className="w-full flex">
 				{[...Array(7)].map((_, idx) => {
-					let className = "grow-1/7 text-center text-white py-1"
+					let className = "w-24 text-center text-white py-1"
 					className += ` ${daysColorPallet[idx]}`
 					return (
 						<h3 key={getDayName(idx)} className={className}>{getDayName(idx)}</h3>
 					)
 				})}
+			</div>
+			<div className=" box-border w-full flex flex-wrap">
+				{dailyTransactions.map( day => (
+					<CalenderDay key={day.id} day={day.day} income={day.income} expense={day.expense} />
+				))}
 			</div>
 		</div>
 	)
--- src/components/CalenderDay.tsx
+++ src/components/CalenderDay.tsx
@@ -1,12 +1,34 @@
 import React from "react";
+import PropTypes from 'prop-types'
 
+type Props = {
+	day: number,
+	income: number,
+	expense: number,
+}
 
-const CalenderDay = () => {
+const CalenderDay = ({day, income, expense}: Props) => {
 	const a = 0
 
+	if (day === 0) {
+		return (
+			<div className="w-24 border" />
+		)
+	}
 	return (
-		<div className="w-20" >hello</div>
+		<div className="w-24 p-1 border">
+			<p className="font-mono">{day}</p>
+			<p className="text-right text-cyan-600">{income}</p>
+			<p className="text-right text-red-600">-{expense}</p>
+			<p className="text-right">{income - expense}</p>
+		</div>
 	)
+}
+
+CalenderDay.propTypes = {
+	day: PropTypes.number.isRequired,
+	income: PropTypes.number.isRequired,
+	expense: PropTypes.number.isRequired
 }
 
 export default CalenderDay
