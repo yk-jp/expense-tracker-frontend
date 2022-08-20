813308250707b741d9b35d32ff87be30458eda3e
Tomo-ja <tomonao.hj@gmail.com>
2022-08-19 17:24:36 -0700

    change each expense font size

--- src/components/CalenderDay.tsx
+++ src/components/CalenderDay.tsx
@@ -12,14 +12,21 @@ const CalenderDay = ({day, income, expense}: Props) => {
 
 	if (day === 0) {
 		return (
-			<div className="w-24 border" />
+			<div className="w-24 border " />
 		)
 	}
+	
+	if(income === 0 && expense === 0){
+		return (
+			<div className="w-24 p-1 border h-24"><p className="font-mono">{day}</p></div>
+		)
+	}
+	
 	return (
-		<div className="w-24 p-1 border">
-			<p className="font-mono">{day}</p>
-			<p className="text-right text-cyan-600">{income}</p>
-			<p className="text-right text-red-600">-{expense}</p>
+		<div className="w-24 p-1 border h-24">
+			<p className="font-mono -mb-3">{day}</p>
+			<p className="text-right text-cyan-600 text-sm">{income}</p>
+			<p className="text-right text-red-600 text-sm">-{expense}</p>
 			<p className="text-right">{income - expense}</p>
 		</div>
 	)
